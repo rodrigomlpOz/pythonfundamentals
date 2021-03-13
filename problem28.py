b292c4818c1f39fa4b0a2931ec98bcf76506b771
rodrigomlpOz <55467806+rodrigomlpOz@users.noreply.github.com>
2021-03-13 00:39:16 -0300

    Update problem28.py

--- problem28.py
+++ problem28.py
@@ -1,4 +1,4 @@
-#create a list of strings (with more than 4)
+#create a list of strings (with more than 4 letters)
 #want you to print the 3 letter of the second string
 
 x = ["apple", "banana", "watermellon", "lemon"]
